1ccb94d99f7dd7f678e04ac3b6d70ed4bf400001
Austin Lieandro <72506433+austinlieandro@users.noreply.github.com>
2023-12-12 12:49:41 +0700

    Todo-App

---
+++ app/src/main/java/com/dicoding/todoapp/ui/add/AddTaskActivity.kt
@@ -0,0 +1,76 @@
+package com.dicoding.todoapp.ui.add
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.dicoding.todoapp.R
+import com.dicoding.todoapp.data.Task
+import com.dicoding.todoapp.ui.ViewModelFactory
+import com.dicoding.todoapp.utils.DatePickerFragment
+import com.google.android.material.textfield.TextInputEditText
+import java.text.SimpleDateFormat
+import java.util.*
+
+class AddTaskActivity : AppCompatActivity(), DatePickerFragment.DialogDateListener {
+    private var dueDateMillis: Long = System.currentTimeMillis()
+
+
+    val addTaskViewModel by viewModels<AddTaskViewModel> {
+        ViewModelFactory.getInstance(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_task)
+
+        supportActionBar?.title = getString(R.string.add_task)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_add, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_save -> {
+                //TODO 12 : Create AddTaskViewModel and insert new task to database
+                val edTitle = findViewById<TextInputEditText>(R.id.add_ed_title).text.toString()
+                val edDescription = findViewById<TextInputEditText>(R.id.add_ed_description).text.toString()
+                val tvDueDate = findViewById<TextView>(R.id.add_tv_due_date).text
+
+                when {
+                    edTitle.isEmpty() -> false
+                    edDescription.isEmpty() -> false
+                    tvDueDate == getString(R.string.due_date) -> false
+                    else -> {
+                        addTaskViewModel.insertTask(Task(0, edTitle, edDescription, dueDateMillis))
+                        finish()
+                        true
+                    }
+                }
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun showDatePicker(view: View) {
+        val dialogFragment = DatePickerFragment()
+        dialogFragment.show(supportFragmentManager, "datePicker")
+    }
+
+    override fun onDialogDateSet(tag: String?, year: Int, month: Int, dayOfMonth: Int) {
+        val calendar = Calendar.getInstance()
+        calendar.set(year, month, dayOfMonth)
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        findViewById<TextView>(R.id.add_tv_due_date).text = dateFormat.format(calendar.time)
+
+        dueDateMillis = calendar.timeInMillis
+    }
+}
---
+++ app/src/main/java/com/dicoding/todoapp/ui/detail/DetailTaskActivity.kt
@@ -0,0 +1,47 @@
+package com.dicoding.todoapp.ui.detail
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.dicoding.todoapp.R
+import com.dicoding.todoapp.data.Task
+import com.dicoding.todoapp.ui.ViewModelFactory
+import com.dicoding.todoapp.ui.add.AddTaskViewModel
+import com.dicoding.todoapp.utils.DateConverter
+import com.dicoding.todoapp.utils.TASK_ID
+import com.google.android.material.textfield.TextInputEditText
+
+class DetailTaskActivity : AppCompatActivity() {
+
+    val detailTaskViewModel by viewModels<DetailTaskViewModel> {
+        ViewModelFactory.getInstance(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_detail)
+
+        //TODO 11 : Show detail task and implement delete action
+        val taskId = intent.getIntExtra(TASK_ID, -1)
+
+        detailTaskViewModel.setTaskId(taskId)
+        detailTaskViewModel.task.observe(this, Observer(this::showDetailTask))
+
+        findViewById<Button>(R.id.btn_delete_task).setOnClickListener {
+            detailTaskViewModel.deleteTask()
+            finish()
+        }
+    }
+
+    private fun showDetailTask(task: Task?) {
+        if (task != null) {
+            findViewById<TextInputEditText>(R.id.detail_ed_title).setText(task.title)
+            findViewById<TextInputEditText>(R.id.detail_ed_description).setText(task.description)
+            findViewById<TextInputEditText>(R.id.detail_ed_due_date)
+                .setText(DateConverter.convertMillisToString(task.dueDateMillis))
+        }
+    }
+}
